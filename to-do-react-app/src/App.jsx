a9ae1edcab2e903c1da19cb86a383b12c76f47e4
Felipe Ramirez <felipe.ramirezrivera@gmail.com>
2022-05-04 10:12:58 -0500

    Worked on the reducer 'add-note' case. The app is now adding a note properly

--- to-do-react-app/src/App.jsx
+++ to-do-react-app/src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
 import StoreProvider from './components/StoreProvider'
 import ListOfToDo from './components/ListOfToDo'
+import Form from './components/Form'
 
 function App() {
 
   return (
     <StoreProvider>
       <h1>Fun To Do List!</h1>
+      <Form/>
+
       <ListOfToDo/>
     </StoreProvider>
      
